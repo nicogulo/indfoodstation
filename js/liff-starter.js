baecbfa89b9f08bb6a10b41596a76494b5fb1b8f
nicogulo <nicogulobelas@gmail.com>
2020-12-14 15:05:32 +0700

    update menu

--- js/liff-starter.js
+++ js/liff-starter.js
@@ -58,39 +58,3 @@ function initializeLiff(myLiffId) {
         });
 }
 
-/**
-* Register event handlers for the buttons displayed in the app
-*/
-function registerButtonHandlers() {
-    // openWindow call
-    document.getElementById('openWindowButton').addEventListener('click', function() {
-        liff.openWindow({
-            url: 'https://indfoodstation.herokuapp.com/',
-            external: true
-        });
-    });
-
-    // closeWindow call
-    document.getElementById('closeWindowButton').addEventListener('click', function() {
-        if (!liff.isInClient()) {
-            sendAlertIfNotInClient();
-        } else {
-            liff.closeWindow();
-        }
-    });
-
-    // sendMessages call
-    document.getElementById('sendMessageButton').addEventListener('click', function() {
-        if (!liff.isInClient()) {
-            sendAlertIfNotInClient();
-        } else {
-            liff.sendMessages([{
-                'type': 'text',
-                'text': "You've successfully sent a message! Hooray!"
-            }]).then(function() {
-                window.alert('Message sent');
-            }).catch(function(error) {
-                window.alert('Error sending message: ' + error);
-            });
-        }
-    });
--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let menu = [
 	{ id: 1, nama: 'Nasi Goreng Padang', kategori: 'food', harga: 15000, foto: 'nasigoreng.jpg'},
 	{ id: 2, nama: 'Sate Madura', kategori: 'food', harga: 20000, foto: 'sate.jpg'},
 	{ id: 3, nama: 'Bakso Goreng', kategori: 'food', harga: 16000, foto: 'baksogoreng.jpg'},
-	{ id: 4, nama: 'Teh Hangat', kategori: 'drink', harga: 4000, foto: 'tehhangat.jpg'},
-	{ id: 5, nama: 'Teh Lemon', kategori: 'drink', harga: 6000, foto: 'tehlemon.jpg'},
+	{ id: 4, nama: 'Teh Hangat', kategori: 'drink', harga: 4000, foto: 'hangattea.jpg'},
+	{ id: 5, nama: 'Teh Lemon', kategori: 'drink', harga: 6000, foto: 'lemontea.jpg'},
 	{ id: 6, nama: 'Es Jeruk', kategori: 'drink', harga: 7000, foto: 'esjeruk.jpg'},
 	{ id: 7, nama: 'Jus Buah Naga', kategori: 'drink', harga: 10000, foto: 'jusbuahnaga.jpg'},
 	{ id: 8, nama: 'Jus Nanas', kategori: 'drink', harga: 10000, foto: 'jusnanas.jpg'}
